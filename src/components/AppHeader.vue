24e89ea5b7980d520eb5e08628c2139745bf1d37
azizafarooq <azizafarooq2000@gmail.com>
2024-02-19 15:55:38 +0100

    feat: AppHeader Banner

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,27 +11,27 @@ export default {
                 {
                     text: 'ABOUT US',
                     url: '#',
-                    current: true,
+                    current: false,
                 },
                 {
                     text: 'FACILITIES',
                     url: '#',
-                    current: true,
+                    current: false,
                 },
                 {
                     text: 'MEMBERSHIP',
                     url: '#',
-                    current: true,
+                    current: false,
                 },
                 {
                     text: 'TESTIMONIALS',
                     url: '#',
-                    current: true,
+                    current: false,
                 },
                 {
                     text: 'BLOG',
                     url: '#',
-                    current: true,
+                    current: false,
                 },
 
             ],
@@ -52,14 +52,29 @@ export default {
             </ul>
         </div>
 
+        <!--Banner-->
+        <div class="banner">
+            <h1>NO PAIN NO GAIN</h1>
+            <p class="mt-3">Motivation is what gets you started. Habit is what keeps you going.</p>
+            <button class="mt-3">Join us today</button>
+        </div>
+
+
+
     </header>
 </template>
 
 <style lang="scss" scoped>
-//Sistemare css
 header {
-    background-color: #1E2123;
-    padding: 3rem;
+    padding: 2.5rem;
+    padding-bottom: 300px;
+    font-family: "Montserrat", sans-serif;
+    background-image: url('/Images/home-banner.jpg');
+    background-size: cover;
+
+    img {
+        cursor: pointer;
+    }
 
     .container {
         display: flex;
@@ -71,6 +86,35 @@ header {
     ul {
         display: flex;
         gap: 10px;
+        font-size: small;
+
+        li {
+            padding: 0.5rem;
+            cursor: pointer;
+        }
+    }
+
+    .banner {
+        color: white;
+        text-align: center;
+
+        h1 {
+            font-weight: bold;
+            font-size: 4.3rem;
+            margin-top: 150px;
+        }
+
+        p {
+            font-weight: lighter;
+        }
+
+        button {
+            background-color: transparent;
+            color: white;
+            border: 0.5px solid #EBEAEA;
+            font-weight: lighter;
+            padding: 5px 20px;
+        }
     }
 }
 </style>
