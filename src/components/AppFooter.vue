9b204dac530405da5614e98b71238e2df94545e4
azizafarooq <azizafarooq2000@gmail.com>
2024-02-19 14:15:39 +0100

    first commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import * as bootstrap from 'bootstrap';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faXmark);
+
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
