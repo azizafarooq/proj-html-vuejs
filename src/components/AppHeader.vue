deb5f38481b88e93372a31b177357e051708013a
azizafarooq <azizafarooq2000@gmail.com>
2024-02-19 14:57:16 +0100

    feat: AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,11 +2,65 @@
 export default {
     data() {
         return {
-        }
-    }
-}
+            links: [
+                {
+                    text: 'HOME',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'ABOUT US',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'FACILITIES',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'MEMBERSHIP',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'TESTIMONIALS',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'BLOG',
+                    url: '#',
+                    current: true,
+                },
+
+            ],
+        };
+    },
+};
 </script>
 
-<template></template>
+<template>
+    <header>
+        <!--Logo-->
+        <img src="../assets/Images/gym_logo_1x.png" alt="">
 
-<style lang="scss" scoped></style>
+        <!--Menù dinamico-->
+        <ul>
+            <li v-for="link in links">{{ link.text }}</li>
+        </ul>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+//Sistemare css
+header {
+    display: flex;
+    justify-content: space-between;
+
+    ul {
+        display: flex;
+        gap: 10px;
+    }
+}
+</style>
